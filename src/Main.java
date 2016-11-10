9831fc661c452b23dbea9fd56a6fed96e13a9567
Elliot Liu (liuliu) <liuliu@cisco.com>
2016-11-10 15:38:20 -0800

    init my java test application

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Java8Tester.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Java8Tester java8Tester = new Java8Tester();
+        java8Tester.testJava8(args);
+        System.out.println("Hello World!");
+    }
+}
